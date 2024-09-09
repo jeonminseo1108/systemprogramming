993028b589c48642f75e8e15023e25d30759725d
jeonminseo <idc08109@naver.com>
2024-09-09 09:52:32 +0000

    0909 find skeleton code and modified

--- assignment1/src/decomment.c
+++ assignment1/src/decomment.c
@@ -10,34 +10,191 @@ functionality, such as processing or transforming the input data.
 In this specific task, the goal is to implement logic that removes 
 C-style comments from the input. */
 
+/* State definition
+ * NORMAL: 
+ * LINE_COMMENT: 
+ * BLOCK_COMMENT: 
+ * STRING: 
+ * CHAR_LITERAL: 
+ */
+enum State {
+	NORMAL,
+	LINE_COMMENT,
+	BLOCK_COMMENT,
+	STRING,
+	CHAR_LITERAL
+};
+
+void handle_normal(char ch, char *prev_char, enum State *state, int *line_cur);
+void handle_block_comment(char ch, char *prev_char, enum State *state, int *line_cur);
+void handle_line_comment(char ch, enum State *state, int *line_cur);
+void handle_string(char ch, enum State *state);
+void handle_char_literal(char ch, enum State *state);
+
 int main(void)
 {
-  // ich: int type variable to store character input from getchar() (abbreviation of int character)
-  int ich;
-  // line_cur & line_com: current line number and comment line number (abbreviation of current line and comment line)
-  int line_cur, line_com;
-  // ch: character that comes from casting (char) on ich (abbreviation of character)
-  char ch;
+	// ich: int type variable to store character input from getchar() (abbreviation of int character)
+	int ich;
+  	// line_cur & line_com: current line number and comment line number (abbreviation of current line and comment line)
+  	int line_cur, line_com;
+  	// ch: character that comes from casting (char) on ich (abbreviation of character)
+  	char ch;
+  	// prev_ch: previous character
+  	char prev_ch;
+	// state: present state
+	enum State state;
 
-  line_cur = 1;
-  line_com = -1;
+	state = NORMAL;
+  	line_cur = 1;
+  	line_com = -1;
+  	prev_ch=0;
 
-  // This while loop reads all characters from standard input one by one
-  while (1) {
-    int got_eof = 0;
+  	// This while loop reads all characters from standard input one by one
+  	while (1) {
+    		int got_eof = 0;
+		
+		ich = getchar();
+    		if (ich == EOF) 
+      			break;
+	
+		ch = (char)ich;
+    		// TODO: Implement the decommenting logic
+		
+    		switch(state){
+			case NORMAL:
+				handle_normal(ch, &prev_char, &state, &line_cur);		
+				break;
+	
+			case BLOCK_COMMENT:
+				handle_block_comment(ch, &prev_char, &state, &line_cur);
+				break;
+	
+			case LINE_COMMENT:
+				handle_line_comment(ch, &state, &line_cur);
+				break;
+	
+			case STRING:
+				handle_string(ch, &state);
+				break;
+	
+			case CHAR_LITERAL:
+				handle_char_literal(ch, &state);
+				break;
+			default: 
+				assert(0);
+				break;	
+    		}	
+	}
+	
+	if (state == BLOCK_COMMENT) {
+        	fprintf(stderr, "Error: line %d: unterminated comment\n", line_com);
+                return(EXIT_FAILURE);
+	}
+	if (prev_char == '/') {
+        	putchar(' ');
+	}
+  	return(EXIT_SUCCESS);
+}
+// NORMAL state handler
+void handle_normal(char ch, char *prev_char, enum State *state, int *line_cur) {
+	if (ch == '/') {
+        	if (*prev_char == '/') {
+            		*state = LINE_COMMENT;
+            		*prev_char = 0;
+        } 
+		else {
+            		*prev_char = ch;
+        	}
+ 	} 
+	else if (ch == '*') {
+        	if (*prev_char == '/') {
+            		*state = BLOCK_COMMENT;
+            		*prev_char = 0;
+        	} 
+		else {
+            		putchar(ch);
+        	}
+    	} 
+	else if (ch == '"') {
+        	*state = STRING;
+        	putchar(ch);
+    	} 
+	else if (ch == '\'') {
+        	*state = CHAR_LITERAL;
+        	putchar(ch);
+    	} 
+	else {
+        	if (*prev_char == '/') {
+            		putchar(' ');
+            		*prev_char = 0;
+        	}
+        	putchar(ch);
+    	}
+	
+    	if (ch == '\n') {
+       		(*line_cur)++;
+    	}
+	return;
+}
 
-    ich = getchar();
-    if (ich == EOF) 
-      break;
+// BLOCK_COMMENT 
+void handle_block_comment(char ch, char *prev_char, enum State *state, int *line_cur) {
+	if (*prev_char == '*' && ch == '/') {
+        	*state = NORMAL;
+        	*prev_char = 0;
+    	} 
+	else if (ch == '*') {
+        	*prev_char = '*';
+    	} 
+	else {
+        	if (ch == '\n') {
+            		putchar('\n');
+            		(*line_cur)++;
+        	} 
+		else {
+            		putchar(' ');
+        	}
+        	*prev_char = 0;
+    	}
+	return;	
+}
 
-    ch = (char)ich;
-    // TODO: Implement the decommenting logic
+// LINE_COMMENT 상태 처리 함수
+void handle_line_comment(char ch, enum State *state, int *line_cur) {
+	if (ch == '\n') {
+        	*state = NORMAL;
+        	putchar('\n');
+        	(*line_cur)++;
+    	}
+	return;
+}
 
-    if (ch == '\n')
-      line_cur++;
-    if (got_eof)
-      break;
-  }
-  
-  return(EXIT_SUCCESS);
+// STRING 상태 처리 함수
+void handle_string(char ch, enum State *state) {
+    	putchar(ch);
+    	if (ch == '\\') {
+        	int ich = getchar();
+        	if (ich != EOF) {
+            		putchar((char)ich);
+        	}
+    	} 
+	else if (ch == '"') {
+        	*state = NORMAL;
+    	}
+	return;
+}
+
+// CHAR_LITERAL 상태 처리 함수
+void handle_char_literal(char ch, enum State *state) {
+    	putchar(ch);
+    	if (ch == '\\') {
+        	int ich = getchar();
+        	if (ich != EOF) {
+            		putchar((char)ich);
+        	}
+    	}
+       	else if (ch == '\'') {
+        	*state = NORMAL;
+    	}
+	return;
 }
